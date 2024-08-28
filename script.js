ee6b150250970eab1febf71bc01c9f034cd5feeb
mohammed-alshaibani <20160129@su.edu.ye>
2024-08-28 11:05:45 +0300

    My Edit in Footer

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
-"use strict";
+'use strict';
 
 const STORE_BASE_URL = "https://fakestoreapi.com";
 const CONTAINER = document.querySelector(".container");
+
+// Navbar Function
+const navBar = () => {
+  const navDiv = document.createElement('div');
+  navDiv.innerHTML = `
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Fake Store</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Category</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#" id="all">All</a></li>
+              <li><a class="dropdown-item" href="#" id="electronics">Electronics</a></li>
+              <li><a class="dropdown-item" href="#" id="jewelery">Jewelery</a></li>
+              <li><a class="dropdown-item" href="#" id="men">Men's clothing</a></li>
+              <li><a class="dropdown-item" href="#" id="women">Women's clothing</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        <a class="cart" href="/card"><i class="bi bi-cart3"></i></a>
+      </div>
+    </div>
+  </nav>`;
+  
+  CONTAINER.appendChild(navDiv);
+};
 
 // Don't touch this function please
 const autorun = async () => {
   const products = await fetchProducts();
   renderProducts(products);
+  renderFooter(); // Render the footer after products
 };
 
 // Don't touch this function please
@@ -64,8 +103,6 @@ const renderProducts = (products) => {
   CONTAINER.appendChild(productRow); // Append the entire row to the container
 };
 
-
-
 // You'll need to play with this function in order to add features and enhance the style.
 const renderProduct = (product) => {
   CONTAINER.innerHTML = `
@@ -74,45 +111,44 @@ const renderProduct = (product) => {
       </div>`;
 };
 
-document.addEventListener("DOMContentLoaded", autorun);
-////////////
-const navBar = () => {
-  CONTAINER.innerHTML = `
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Fake Store</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Category</a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#" id="all">All</a></li>
-            <li><a class="dropdown-item" href="#" id="electronics">Electronics</a></li>
-            <li><a class="dropdown-item" href="#" id="jewelery">Jewelery</a></li>
-            <li><a class="dropdown-item" href="#" id="men">Men's clothing</a></li>
-            <li><a class="dropdown-item" href="#" id="women">Women's clothing</a></li>
-          </ul>
-        </li>
-        
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-      <a class="cart" href="/card"><i class="bi bi-cart3"></i></a>
+// Function to render the footer
+const renderFooter = () => {
+  const footer = document.createElement('footer');
+  footer.className = 'bg-dark text-white text-center py-3';
+
+  footer.innerHTML = `
+  <section class="">
+  <!-- Footer -->
+  <footer class="text-center text-white" style="background-color: #0a4275;">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: CTA -->
+      <section class="">
+        <p class="d-flex justify-content-center align-items-center">
+          <span class="me-3">Register for free</span>
+          <button data-mdb-ripple-init type="button" class="btn btn-outline-light btn-rounded">
+            Sign up!
+          </button>
+        </p>
+      </section>
+      <!-- Section: CTA -->
     </div>
-  </div>
-</nav>`;
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2020 Copyright:Mo & Ahmed
+    </div>
+    <!-- Copyright -->
+  </footer>
+  </section>
+
+  `;
+
+  CONTAINER.appendChild(footer);
 };
 
+// Initial call to add navbar
 navBar();
-// navBar
 
-
+document.addEventListener("DOMContentLoaded", autorun);
